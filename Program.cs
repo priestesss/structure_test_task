04d0e7c3613bdc0b8deed8757a131eecfb22d3e5
priestesss <94913660+priestesss@users.noreply.github.com>
2021-12-08 21:42:36 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,27 +11,55 @@ namespace struct_test_task1
             int help;
             for (int i = 0; i < 10; i++)
             {
-                mas[i].is_predator = Convert.ToBoolean(ran.Next(0,1));
+                mas[i].is_predator = Convert.ToBoolean(ran.Next(-1,2));
                 mas[i].weight = ran.Next(38,114);
+            }
+            Console.WriteLine("is predator:");
+            for (int i = 0; i < 10; i++)
+            {
+               Console.Write(mas[i].is_predator ? " 1" : " 0");
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < 10-1; i++)
+            {
+                if (mas[i].is_eaten)
+                {
+                    continue;
+                }
+                if (mas[i].is_predator == true)
+                {
+                    if (mas[i + 1].is_predator == false)
+                    {
+                        mas[i + 1].is_eaten = true;
+                        
+                        
+                    }
+                    else if (mas[i].weight > mas[i + 1].weight)
+                    {
+                        mas[i + 1].is_eaten = true;
+                    }
+                    else 
+                    {
+                        mas[i].is_eaten = true;
+                    }
+                    
+                }
+
             }
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 10; j++)
-                {
-                    if (mas[i].is_predator[i] == true && mas[i].is_predator[j] == false)
-                    {
-                        help = mas[i];
-                        mas[i] = mas[j];
-                        mas[j] = help;
-                    }
-                }
+                Console.Write(mas[i].is_eaten ? " *" : " -");
             }
+            
         }
         struct Spore
         {
             public bool is_predator;
             public int weight;
+            public bool is_eaten;
 
         }
     }
+
+    
 }
